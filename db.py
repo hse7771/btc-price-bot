a7bfe47de26820cfeeee1ae1d59623eed4c8ca49
hse7771 <kab20050703@gmail.com>
2025-04-07 17:27:54 -0400

    feat(db): add functions to interact with db
    add save preferences function to db
    add load preferences function to db
    add clear preferences function to db

--- db.py
+++ db.py
@@ -12,3 +12,29 @@ async def init_db():
             )
         ''')
         await db.commit()
+
+
+async def save_user_currencies(user_id: int, currencies: list[str]):
+    currency_str = ",".join(currencies)
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute(
+            "REPLACE INTO currency_preferences (user_id, currencies) VALUES (?, ?)",
+            (user_id, currency_str)
+        )
+        await db.commit()
+
+async def load_user_currencies(user_id: int) -> list[str] | None:
+    async with aiosqlite.connect(DB_NAME) as db:
+        async with db.execute(
+            "SELECT currencies FROM currency_preferences WHERE user_id = ?",
+            (user_id,)
+        ) as cursor:
+            row = await cursor.fetchone()
+            if row and row[0]:
+                return row[0].split(",")
+            return None
+
+async def clear_user_currencies(user_id: int):
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute("DELETE FROM currency_preferences WHERE user_id = ?", (user_id,))
+        await db.commit()
